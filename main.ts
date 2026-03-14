0b64fab7eec36dedd865dd99280a281dc52c7b28
juananrodfer-dot <juananrodfer@gmail.com>
2026-03-14 20:51:19 +0100

    tercer nivel

--- main.ts
+++ main.ts
@@ -5,10 +5,44 @@ function CargaNivel (n: number) {
     tiles.setCurrentTilemap(tilemap`nivel2`)
     tiles.placeOnRandomTile(jugador, sprites.dungeon.doorClosedNorth)
     scene.cameraFollowSprite(jugador)
+    niño_maléfico = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f a a f f f . . . . 
+        . . . f f f a a a a f f f . . . 
+        . . f f f c c c c c c f f f . . 
+        . . f f c a a a a a a c c f . . 
+        . . f c a f f f f f f a c f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f c c c c c c f 4 e . . 
+        . . 4 d f c c c c c c f d 4 . . 
+        . . 4 4 f 4 4 a a 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Enemy)
+    niño_maléfico.setPosition(66, 33)
+    niño_maléfico.follow(jugador, 20)
+}
+function nivel3 (núm: number) {
+    tiles.setCurrentTilemap(tilemap`nivel0`)
+    tiles.placeOnRandomTile(jugador, sprites.dungeon.stairLadder)
+    controller.moveSprite(jugador, 100, 0)
+    jugador.ay = 400
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
     sprites.destroy(projectile)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jugador.isHittingTile(CollisionDirection.Bottom)) {
+    	
+    }
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`EXIT`, function (sprite, location) {
     CargaNivel(1)
@@ -16,7 +50,11 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`EXIT`, function (sprite, loca
 scene.onOverlapTile(SpriteKind.Player, assets.tile`miMosaico`, function (sprite, location) {
     puededisparar = 0
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenNorth, function (sprite, location) {
+    nivel3(1)
+})
 let projectile: Sprite = null
+let niño_maléfico: Sprite = null
 let jugador: Sprite = null
 let puededisparar = 0
 let nivel = 1
@@ -42,8 +80,25 @@ jugador = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(jugador)
 scene.cameraFollowSprite(jugador)
-let niño_malo = sprites.create(assets.image`niño malo`, SpriteKind.Enemy)
-info.setLife(13)
+let niño_malo = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f a a f f f . . . . 
+    . . . f f f a a a a f f f . . . 
+    . . f f f c c c c c c f f f . . 
+    . . f f c a a a a a a c c f . . 
+    . . f c a f f f f f f a c f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f c c c c c c f 4 e . . 
+    . . 4 d f c c c c c c f d 4 . . 
+    . . 4 4 f 4 4 a a 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Enemy)
+info.setLife(15)
 game.onUpdateInterval(1, function () {
     if (puededisparar) {
         projectile = sprites.createProjectileFromSprite(img`
@@ -52,12 +107,12 @@ game.onUpdateInterval(1, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 4 4 . . . . . . . 
-            . . . . . . 4 5 5 4 . . . . . . 
-            . . . . . . 2 5 5 2 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+            . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+            . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+            . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+            . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+            . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
