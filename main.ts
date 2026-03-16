d6d11e317da77bb05f673c678e1dedc0e5794e07
juananrodfer-dot <juananrodfer@gmail.com>
2026-03-16 21:44:30 +0100

    fantasma azul

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const lava = SpriteKind.create()
+    export const BlueGhost = SpriteKind.create()
 }
 function CargaNivel (n: number) {
     sprites.destroy(niño_malo)
@@ -140,12 +141,41 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`baston`, function (sprite, lo
     if (tienebaston == 0) {
         tienebaston = 1
         puededisparar = 0
-        game.showLongText("\"CONSEGUIDO BASTON\"", DialogLayout.Top)
+        music.setVolume(255)
+        music.play(music.createSoundEffect(WaveShape.Square, 1, 1348, 255, 255, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     }
 })
 function nivel3 (núm: number) {
     sprites.destroy(fantasma)
     sprites.destroy(fantasmita)
+    fantasma_azul = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff9999ff........
+        .......fb998899bf.......
+        .......f99899899f.......
+        ......f6998998996f......
+        ......f6999889996f......
+        ......f6669999666f......
+        ......f66bfddfb66f......
+        ......f66cf99fc66f......
+        .....ffff9999996f.......
+        ....f6999668986fff......
+        ....f969668f8f89996f....
+        ....f6f6f6ffff96969f....
+        .........fffffff6f6f....
+        ..........fffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.BlueGhost)
+    fantasma_azul.setPosition(99, 63)
+    fantasma_azul.follow(jugador, 30)
     tiles.setCurrentTilemap(tilemap`nivel0`)
     tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
     controller.moveSprite(jugador, 100, 0)
@@ -155,9 +185,14 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, ot
     info.changeLifeBy(-1)
     sprites.destroy(projectile)
 })
+sprites.onOverlap(SpriteKind.BlueGhost, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`miMosaico6`, function (sprite, location) {
     music.setVolume(167)
-    music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G B A G C5 B A B ", 400), music.PlaybackMode.UntilDone)
+    CargaNivel4(1)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (jugador.isHittingTile(CollisionDirection.Bottom)) {
@@ -165,6 +200,49 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
     music.play(music.createSoundEffect(WaveShape.Square, 405, 641, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
+function CargaNivel4 (núm: number) {
+    tiles.setCurrentTilemap(tilemap`nivel7`)
+    jugador = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    tiles.placeOnRandomTile(jugador, assets.tile`baseTransparency16`)
+    niño_super_malefico = sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Enemy)
+    sprites.destroy(fantasma_azul)
+    sprites.destroy(fantasmon)
+    niño_super_malefico.follow(jugador)
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     info.changeLifeBy(-1)
     tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
@@ -181,8 +259,13 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`EXIT`, function (sprite, loca
 scene.onOverlapTile(SpriteKind.Player, assets.tile`miMosaico0`, function (sprite, location) {
     if (tienemedicina == 0) {
         tienemedicina = 1
+        music.setVolume(255)
+        music.play(music.createSoundEffect(WaveShape.Square, 1, 1348, 255, 255, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         game.showLongText("medicina conseguida", DialogLayout.Top)
     }
+})
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`miMosaico7`, function (sprite, location) {
+	
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenNorth, function (sprite, location) {
     nivel3(1)
@@ -190,7 +273,9 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenNorth, function (
     music.play(music.stringPlayable("G B A G C5 B A B ", 400), music.PlaybackMode.UntilDone)
 })
 let tienemedicina = 0
+let niño_super_malefico: Sprite = null
 let projectile: Sprite = null
+let fantasma_azul: Sprite = null
 let puededisparar = 0
 let nivel = 0
 let fantasmita: Sprite = null
